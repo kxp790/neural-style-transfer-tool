72e42350c7fd438bde9342017d9c769aa26ddd8a
Killu-Smilla Palk <kxp790@cs.bham.ac.uk>
2021-03-31 15:01:24 +0300

    Add PyMongo database script

---
+++ api/data/database.py
@@ -0,0 +1,35 @@
+import pymongo
+
+mongodb = pymongo.MongoClient()
+
+style_transfer_db = mongodb['styleTransferDB']
+
+designs_collection = style_transfer_db['designs']
+pins_collection = style_transfer_db['pins']
+
+def make_sample_data():
+    designs_data = {
+        'id' : 'af2iak2jh3',
+        'style_image_id' : 3,
+        'layers' : {
+            'block1_conv1': 0.2,
+            'block2_conv1': 0.3,
+            'block3_conv1': 0.3,
+            'block4_conv1': 0.10,
+            'block5_conv1': 0.15
+        },
+        'content_wight' : 20,
+        'style_weight' : 30
+    }
+    designs_collection.insert_one(designs_data)
+
+    pins_data = {
+        'id' : 'af2iak2jh3',
+        'pin' : 1234
+    }
+    pins_collection.insert_one(pins_data)
+
+def get_model_design_data(model_design_id):
+    for entry in designs_collection.find({'id': model_design_id}):
+        print(entry)
+        return(entry)
