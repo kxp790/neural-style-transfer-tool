7f4f6fe6eb9151a9b2d911209992f3aa09b172ef
Killu-Smilla Palk <kxp790@cs.bham.ac.uk>
2021-04-01 13:06:28 +0300

    Add context to display data on all components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import Header from './Header';
+import Header from './components/Header';
 import HomePage from './components/HomePage';
 import ModelDesignPage from './components/ModelDesignPage';
 import ResultPage from './components/ResultPage';
 import SupportPage from './components/SupportPage';
+import LayerSelectionItem from './components/LayerSelectionItem';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AppContext } from './components/AppContext';
 
 function App() {
+  const [design, setDesign] = useState();
+  const layers = ['conv_1_1', 'conv_1_2', 'pooling_1',
+                  'conv_2_1', 'conv_2_2', 'pooling_2',
+                  'conv_3_1', 'conv_3_2', 'conv_3_3', 'pooling_3',
+                  'conv_4_1', 'conv_4_2', 'conv_4_3', 'pooling_4',
+                  'conv_5_1', 'conv_5_2', 'conv_5_3', 'pooling_5']
+
+  useEffect(() => {
+    fetch("/design/af2iak2jh3").then(response =>
+      response.json().then(data => {
+        setDesign(data);
+      })
+    );
+  }, []);
+
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route path="/" exact component={HomePage}/>
-          <Route path="/model" component={ModelDesignPage}/>
-          <Route path="/result" component={ResultPage}/>
-          <Route path="/support" component={SupportPage}/>
-        </Switch>
-      </div>
+      <AppContext.Provider value={{design, layers}}>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/" exact children={<HomePage design={design} />}/>
+            <Route path="/model" exact component={ModelDesignPage}/>
+            <Route path="/model/layers" exact children={<LayerSelectionItem design={design, layers} />} />
+            <Route path="/result" component={ResultPage}/>
+            <Route path="/support" component={SupportPage}/>
+          </Switch>
+        </div>
+      </AppContext.Provider>
     </BrowserRouter>
   );
 }
 
-export default App;
-
-// export default (
-//   <Route path="/" component={App}>
-//     <IndexRoute component={HomePage} />
-//     <Route path="/model_design" component={ModelDesignPage} />
-//     <Route path="/result" component={ResultPage} />
-//   </Route>
-// );
+export default App;  
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom'; 
+import { AppContext } from './AppContext';
 
-function  HomePage() {
+export const HomePage = () => {
+    const design = useContext(AppContext);
+    console.log(design)
     return(
-        <div>
-            <Link to="/model" className="btn">GO</Link>
+        <div class="button-holder">
+            <Link to="/model" className="button button1">Start</Link>
+            <p className="pad">or</p>
+            <Link to="/model" className="button button2">Continue</Link>
         </div>
     )  
 }
