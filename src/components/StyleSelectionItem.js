808f83a24c1a6cfc86165350012da380ad670acf
Killu-Smilla Palk <kxp790@cs.bham.ac.uk>
2021-04-03 21:48:32 +0300

    Test selector for style images

--- src/components/StyleSelectionItem.js
+++ src/components/StyleSelectionItem.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import style_1 from '../images/placeholder.png'
-import style_2 from '../images/stained-glass.jpg'
-import style_3 from '../images/placeholder.png'
+import Select from 'react-select';
+import style_1 from '../images/placeholder.png';
+import style_2 from '../images/stained-glass.jpg';
+import style_3 from '../images/placeholder.png';
 
-const StyleSelectionItem = () => (
-    <div className="pad-style-image">
-        <div><br></br></div>
-        <div><br></br></div>
-        <div><br></br></div>
-        <img src={style_1} className="style-select-image"/>
-        <img src={style_2} className="style-select-image"/>
-        <img src={style_3} className="style-select-image"/>
-    </div>
-)
+const StyleSelectionItem = () => {
+    const styles = [
+        { value: 'style_1', label: <div><img src={style_1} height="100vh" width="100vh"/></div> },
+        { value: 'style_2', label: <div><img src={style_2} height="100vh" width="100vh"/></div> },
+        { value: 'style_3', label: <div><img src={style_3} height="100vh" width="100vh"/></div> },
+    ];
+    
+    return(
+        <div className="pad-style-image">
+            <div><br></br></div>
+            <div><br></br></div>
+            <div><br></br></div>
+            <Select className="select-style" options={styles}/>  
+            <img src={style_1} className="style-select-image"/>
+            <img src={style_2} className="style-select-image"/>
+            <img src={style_3} className="style-select-image"/>
+
+             
+        </div>
+    )
+}
 
 export default StyleSelectionItem
