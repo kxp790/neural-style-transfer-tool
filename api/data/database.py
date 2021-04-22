f2c902635d323b9a9615b79e6765f731ccb1f65c
Killu-Smilla Palk <kxp790@cs.bham.ac.uk>
2021-04-22 20:40:11 +0100

    Update database script

--- api/data/database.py
+++ api/data/database.py
@@ -6,29 +6,3 @@ style_transfer_db = mongodb['styleTransferDB']
 
 designs_collection = style_transfer_db['designs']
 pins_collection = style_transfer_db['pins']
-
-def make_sample_data():
-    designs_data = {
-        'id' : 'af2iak',
-        'style_image_name' : 'stained-glass.jpg',
-        'content_layer' : 'block4_conv2',
-        'style_layers' : {
-            'block1_conv1': 0.2,
-            'block2_conv1': 0.3,
-            'block3_conv1': 0.3,
-            'block4_conv1': 0.10,
-            'block5_conv1': 0.15
-        },
-        'content_wight' : 20,
-        'style_weight' : 30,
-        'iterations' : 50
-    }
-    designs_collection.insert_one(designs_data)
-
-    pins_data = {
-        'id' : 'af2iak2jh3',
-        'pin' : 1234
-    }
-    pins_collection.insert_one(pins_data)
-
-make_sample_data()
