bc91466130ee2fcaadbe1a071ea6ac1e531ec4da
Killu-Smilla Palk <kxp790@cs.bham.ac.uk>
2021-04-28 22:11:08 +0100

    Update logo div in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import logo from '../images/logo.png'
 
 const Header = () => (
     <div className='App-header'>
-        <Link to='/'><img src={logo} className="App-logo logo" alt="logo" /></Link>
+        <div style={{padding: "0.5vh 10vw"}}>
+            <Link to='/'><img src={logo} className="App-logo logo" alt="logo" /></Link>
+        </div>
+        
         {/* <p className='header-links title'>PHOTO STYLER</p> */}
         <Link to='/support' className='header-links'>Support</Link>
     </div>
