c24cb1fbf48a210705ab037915843f8226f4ba7a
Killu-Smilla Palk <kxp790@cs.bham.ac.uk>
2021-04-29 00:23:58 +0100

    Add validation to the new design page

--- src/components/NewDesignPage.js
+++ src/components/NewDesignPage.js
@@ -1,4 +1,6 @@
-import React, { useContext, useEffect } from 'react'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState, useContext, useEffect } from 'react'
 import PinInput from 'react-pin-input'
 import { withRouter } from 'react-router-dom'
 import axios from 'axios'
@@ -8,6 +10,10 @@ import { AppContext } from './AppContext'
 const NewDesignPage = (props) => {
     const { setDesign } = useContext(AppContext)
 
+    const [ inputPin, setInputPin ] = useState(1234)
+    const [ inputPinIsValid, setInputPinIsValid ] = useState(true)
+    const pinExp = new RegExp('^[0-9]{4}$');
+
     // function to call api to save new pin to database
     const savePin = () => {
         props.history.push("/model")
@@ -16,6 +22,11 @@ const NewDesignPage = (props) => {
     useEffect(() => {
         makeNewDesign()
     }, [])
+
+    const updateInputPin = (value) => {
+        setInputPinIsValid(pinExp.test(value))
+        setInputPin(value)
+    }
 
     // function to call api to make a new design, load it into context and and redirects to model
     async function makeNewDesign () {
@@ -35,26 +46,26 @@ const NewDesignPage = (props) => {
                 {({design}) => (
                     design === undefined ? <PropogateLoader /> :
                     <>
-                        <h4 className="caption-box">YOUR DESIGN ID:</h4>
+                        <h4 className="caption-box">THIS IS YOUR DESIGN ID</h4>
                         <div style={{paddingBottom: "1vh"}}>
                             <p className="input-text-box">{design.id}</p>
                         </div>
-                        <h4 className="caption-box">CHOOSE A PIN:</h4>
+                        <h4 className="caption-box">CHOOSE A PIN {(!inputPinIsValid) ? <FontAwesomeIcon icon={faTimes} style={{display: "inline", color: "red", padding:"0px 1px"}} /> : ''}</h4>
                         <PinInput 
                             className="pad-pin"
                             length={4} 
                             initialValue="1234"
-                            onChange={(value, index) => {}} 
+                            onChange={(value) => updateInputPin(value)}
                             type="numeric" 
                             inputMode="number"
                             style={{paddingBottom: "3vh", width: "25vh", minWidth: "fit-content", margin: "0 auto"}}
                             inputStyle={{borderRadius: "4px", border: "2px solid #374a5c", backgroundColor: "#9db0c2", fontSize: "22px", height: "5vh", width: "5vh", padding: "1vh"}}
                             inputFocusStyle={{borderRadius: "4px", backgroundColor: "#52708a"}}
-                            onComplete={(value, index) => {}}
+                            onComplete={(value) => updateInputPin(value)}
                             autoSelect={true}
                             regexCriteria={/^[0-9]*$/}
                         />
-                        <input type="submit" to="/model" className="button button2" value="Continue" onClick={savePin} />
+                        <button type="submit" to="/model" className="button button2" disabled={ !inputPinIsValid} onClick={savePin}>Continue</button>
                     </>
                 )}
             </AppContext.Consumer>
